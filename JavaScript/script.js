046fea7b51d9e2255d7a865c9a6fb034da7df1a2
Nyx-Nightmare <assel.fatayerji201@gmail.com>
2024-03-04 04:54:27 +0200

    Third commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -18,3 +18,38 @@ function changePicture() {
   i++;
   
 }
+
+window.onload = function (){
+    increaseNumberAnimation("community",3500);
+    increaseNumberAnimation("events",1535);
+    increaseNumberAnimation("orgs",1535);
+    increaseNumberAnimation("industries",60);
+}
+function increaseNumberAnimation(elementId, endNumber, speed = 5) {
+    const element = document.getElementById(elementId)
+    
+    if(!element) return
+    
+    /* A dataset variable that is added to the animated element*/
+    const animationRunning = JSON.parse(element.dataset.animationRunning ?? false)
+    
+    if(animationRunning) return
+    
+    element.dataset.animationRunning = true
+    
+    incNbrRec(0, endNumber, element, speed)
+  }
+  
+  /*A recursive function to increase the number.*/
+  function incNbrRec(currentNumber, endNumber, element, speed) {
+    if (currentNumber <= endNumber) {
+      element.innerHTML = currentNumber
+      setTimeout(function() {
+        incNbrRec(currentNumber + 1, endNumber, element, speed)
+      }, speed) //Delay a bit before calling the function again.
+    } else {
+        let temp =element.innerHTML;
+        element.innerHTML = temp + "+";
+      element.dataset.animationRunning = false
+    }
+  }
